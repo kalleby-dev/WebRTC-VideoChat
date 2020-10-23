44665f86890d2a025e392218d5d2c83f9a51a688
kalleby <kalleby.santos38@gmail.com>
2020-10-23 22:19:11 +0100

    feat: Get user video

--- sender/sender.js
+++ sender/sender.js
@@ -6,8 +6,40 @@ function onBtnSend(){
   sendData({ type: "store_user" });
 }
 
+function onBtnCall(){
+  document.querySelector(".video-grid").style.display = "block";
+  startStream();
+}
+
 // Envia informações para o servidor Socket
 function sendData(data){
   data.username = username;
   webSocket.send(JSON.stringify(data));
+  console.log("Enviando: " + username + " para o socket");
+}
+
+// Recebe e envia os dados da video-chamada
+let localStream;
+function startStream(){
+  console.log("Inciando chamada");
+
+  navigator.mediaDevices.getUserMedia({
+    video:{
+      frameRate: 30,
+      width: { min: 400, ideal: 720, max: 1280 },
+      aspectRatio: 1.3333
+    },
+    audio: true
+  })
+  .then(stream => {
+    localStream = stream;
+    playLocalVideo(localStream);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+}
+
+function playLocalVideo(stream){
+  document.querySelector("#local-video").srcObject = stream;
 }
